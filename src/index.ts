cdd50a3bcee0f3395e15bb3d4ed5226b8e44c135
DimaGashko <dgashko@mail.ua>
2019-02-05 20:44:37 +0200

    initApp

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,41 @@
 import './styles/index.sass';
+import Vector from './components/Vector/Vector';
 
-console.log('Perlin Noise');
+const canvas = <HTMLCanvasElement>document.querySelector('.canvas');
+const ctx = canvas.getContext('2d');
+
+let size = new Vector(100, 100);
+
+updateSize();
+initEvents();
+
+draw();
+
+function draw() {
+   ctx.fillRect(-100, -100, 200, 200);
+}
+
+function onResize() {
+   updateSize();
+   draw();
+}
+
+function updateSize() {
+   size = new Vector(window.innerWidth, window.innerHeight);
+
+   canvas.width = size.x;
+   canvas.height = size.y;
+
+   setStartTransforms();
+}
+
+function setStartTransforms() {
+   ctx.translate(size.x / 2, size.y / 2);
+}
+
+function initEvents() {
+   window.addEventListener('resize', () => {
+      onResize();
+   });
+}
+
